a129d5fa9b4af79900c0cc9607297f86c1f78627
Letícia Luzia <leticialuzia@icloud.com>
2021-02-09 22:14:10 -0300

    Changing server.listen to app.listen

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
