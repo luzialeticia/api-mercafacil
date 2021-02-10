866078f5a349a6b06530822de5d0d52e044141df
Letícia Luzia <leticialuzia@icloud.com>
2021-02-09 22:02:40 -0300

    Importing app to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const app = require('./src/app')
 const PORT = process.env.PORT || 8080;
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
