d42b2ccd026ef18482753932216de4fcf18ccdf3
Letícia Luzia <leticialuzia@icloud.com>
2021-02-10 01:04:00 -0300

    Creating and exporting pool

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 const dotEnv = require('dotenv');
 
-dotEnv.config()
+dotEnv.config();
 
 const pool = mysql.createPool({
     "user": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
-})
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
+});
+
+exports.pool = pool;
