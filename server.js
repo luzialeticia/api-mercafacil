2d705eed1622d3d237a6cdf3d2ec9c5a0cc0cc76
Letícia Luzia <leticialuzia@icloud.com>
2021-02-09 22:13:08 -0300

    Deleting the http stuff

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const http = require('http');
 const app = require('./src/app')
 const PORT = process.env.PORT || 8080;
-const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
