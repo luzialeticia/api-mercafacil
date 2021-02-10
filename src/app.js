be9c42e419a57938a77e72182acaf0ffb1bd5e75
Letícia Luzia <leticialuzia@icloud.com>
2021-02-09 22:26:49 -0300

    Importing and using index route

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const index = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use('/', cors(), index)
 
 module.exports = app;
