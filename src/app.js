455cd56faacb416891813c5fda233670ae5bc781
Letícia Luzia <leticialuzia@icloud.com>
2021-02-09 22:11:43 -0300

    Creating variables importing cors and body-parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
-app.use((req, res) => {
-    res.status(200).send({
-        message: 'OK!'
-    });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 module.exports = app;
