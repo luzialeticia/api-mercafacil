29ce15e953d1e1fcff109a4a7176425dc4a5b2cd
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 18:13:05 -0300

    Importing Client route

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ const app = express();
 require('./database/macapa');
 
 const index = require('./routes/index');
+const clientRoute = require('./routes/Clients');
 const macapaRoutes = require('./routes/Macapa');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
 app.use('/', cors(), index);
+app.use('/signup', cors(), clientRoute);
 app.use('/macapa', cors(), macapaRoutes);
 
 module.exports = app;
