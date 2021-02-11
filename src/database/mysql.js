afb0e31f1d409458760cc3e56ae074402c273a4d
Letícia Luzia <leticialuzia@icloud.com>
2021-02-10 23:09:46 -0300

    Semicolons

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,10 +2,10 @@ const con = require('../config/mysql');
 
 con.connect((err) => {
     if (err) {
-        console.log('Erro connecting to database...', err)
-        return
+        console.log('Erro connecting to database...', err);
+        return;
     }
-    console.log('Connection established!')
+    console.log('Connection established!');
 });
 
 module.exports = con;
