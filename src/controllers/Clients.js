5ab509cb5574c5b8674e98f636b406c71e87acd2
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 18:07:56 -0300

    Ereasing password_hash from controller

--- src/controllers/Clients.js
+++ src/controllers/Clients.js
@@ -1,21 +1,14 @@
 const Client = require('../models/Clients');
-const hash = require('bcrypt');
 
 module.exports = {
     async store(req, res) {
-        const { password } = req.body;
-
-        const passwordHash = await hash(password, 8);
-
-        req.body.password_hash = passwordHash;
-
         const { name } = req.body;
 
-        const contact = await Client.create({ name });
+        const client = await Client.create({ name });
 
         return res.status(201).json({
             message: "Cliente cadastrado com sucesso!",
-            contact
+            client
         });
     }
 };
