f680d7ff86ecbccb01f5c5af6b591bd44ebf044e
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 00:45:18 -0300

    Configuring migrations and data types

---
+++ src/database/migrations/20210211032731-create-contacts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('contacts',{
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      celular: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      }
+    });
+  },
+
+  down: async(queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
