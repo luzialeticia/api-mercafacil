0e79d145d03bad8159ec664a85a328dbb8e6bbab
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 01:19:56 -0300

    Rename mysql.js to macapa.js and editing the imports for this archive

---
+++ src/database/macapa.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/macapa');
+
+const Contact = require('../models/Macapa');
+
+const connection = new Sequelize(dbConfig);
+
+Contact.init(connection);
+
+module.exports = connection
--- src/database/mysql.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const dbConfig = require('../config/mysql');
-
-const connection = new Sequelize(dbConfig);
-
-module.exports = connection
