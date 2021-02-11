6db5ab37f8a7964645c9391de2e380bfc14ae928
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 18:29:11 -0300

    trying to generate password with bcrypt

--- src/controllers/Clients.js
+++ src/controllers/Clients.js
@@ -1,7 +1,14 @@
 const Client = require('../models/Clients');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async store(req, res) {
+        const { password } = req.body;
+
+        const passwordHash = bcrypt.hash(password, 8);
+
+        req.body.password_hash = passwordHash;
+
         const { name } = req.body;
 
         const client = await Client.create({ name });
--- src/models/Clients.js
+++ src/models/Clients.js
@@ -1,22 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 
 class Client extends Model {
     static init(sequelize) {
         super.init({
             name: DataTypes.STRING(200),
-            password: DataTypes.VIRTUAL,
             password_hash: DataTypes.STRING(20)
         }, {
             sequelize,
             modelName: 'client'
         });
-        this.addHook('beforeSave', async client => {
-            if(client.password) {
-                client.password_hash = await bcrypt.hash(client.password, 8);
-            }
-        });
-        return this;
     };
 };
 
