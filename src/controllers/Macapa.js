fc34e07ff98f198a0e02e27279e4b6193c6997a0
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 02:52:10 -0300

    Name of colunms in english

--- src/controllers/Macapa.js
+++ src/controllers/Macapa.js
@@ -2,9 +2,9 @@ const Contact = require('../models/Macapa');
 
 module.exports = {
     async store(req, res) {
-        const { nome, celular } = req.body;
+        const { name, cellphone } = req.body;
 
-        const contact = await Contact.create({ nome, celular });
+        const contact = await Contact.create({ name, cellphone });
 
         return res.status(201).json({
             message: "Contato cadastrado com sucesso!",
