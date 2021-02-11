f212049fc8743cb0359643fd895c96068f051d84
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 01:48:05 -0300

    Connecting macapa routes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+require('./database/macapa');
+
 const index = require('./routes/index');
+const macapaRoutes = require('./routes/Macapa');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/', cors(), index);
+app.use('/macapa', cors(), macapaRoutes);
 
 module.exports = app;
