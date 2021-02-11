74f8fc5d5e4144d84a22376131fe7eb0174a290a
Letícia Luzia <leticialuzia@icloud.com>
2021-02-11 18:02:16 -0300

    Creating hash for password

--- src/controllers/Clients.js
+++ src/controllers/Clients.js
@@ -3,6 +3,12 @@ const hash = require('bcrypt');
 
 module.exports = {
     async store(req, res) {
+        const { password } = req.body;
+
+        const passwordHash = await hash(password, 8);
+
+        req.body.password_hash = passwordHash;
+
         const { name } = req.body;
 
         const contact = await Client.create({ name });
